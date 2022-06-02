9c932afa6695c7b9372da016c3c3cebd1dcda8f0
lplatzer <dev@luisplatzer.de>
2022-06-02 15:21:23 +0200

    Added & finished layout
    created overall page layout

---
+++ app/routes/blog/$slug.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export default function BlogSlug() {
+
+    return(
+        <h1>Post slug</h1>
+    )
+}
---
+++ app/routes/blog/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function Blog() {
+    return (
+        <h1>Blog page</h1>
+    )
+}
